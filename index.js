ba8cf4f7d14f2e16cdb08a610508a0c56294eeab
tiennhot23 <45113732+tiennhot23@users.noreply.github.com>
2021-06-21 21:35:12 +0700

    add serverless-http

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const PORT = process.env.PORT || 3000
 const chapter = require('./routers/chapter')
 const comic = require('./routers/comic')
 const mongoose = require('mongoose')
+const serverless = require('serverless-http')
 require('dotenv').config()
 
 const app = express()
@@ -17,4 +18,5 @@ mongoose.connect(process.env.DATABASE_URL, {
 app.use('/api/', comic)
 app.use('/api/chapter', chapter)
 
-app.listen(PORT, console.log("Listening on port: " + `${PORT}`))
+module.exports.handler = serverless(app)
+// app.listen(PORT, console.log("Listening on port: " + `${PORT}`))
